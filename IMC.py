f80181510c964bb72c47ba51c2fd780034625786
16bitsvnm66 <f.t.saldanha@gmail.com>
2026-05-29 11:50:25 +0100

    Peso
    Verificar o peso e atribuir uma classificação

--- IMC.py
+++ IMC.py
@@ -7,3 +7,12 @@ def calcular_imc(peso: float, altura_cm: float) -> float:
     return imc
 
 
+def verificar_peso(imc: float) -> str:
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif 18.5 <= imc < 25:
+        return "Peso normal"
+    elif 25<= imc < 30:
+        return "Sobrepeso"
+    else: 
+        return "Obesidade"
